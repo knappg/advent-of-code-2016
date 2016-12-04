78b4a510644dc9bffce53f2415304793656d1f1e
Galen Knapp <galen.knapp@redfin.com>
2016-12-04 11:59:20 -0800

    Cleanup

--- day-2/day-2.go
+++ day-2/day-2.go
@@ -32,32 +32,21 @@ func retrieveInput() []string {
 }
 
 func processMove(move string, curPoint Point, keypad [][]string) Point {
-	// fmt.Printf("Processing move: %v\n", move)
-	// fmt.Printf("Previous point: (%v, %v)\n", curPoint.x, curPoint.y)
 	newX, newY := curPoint.x, curPoint.y
 	width, height := len(keypad[0]), len(keypad)
 	switch move {
 	case "U":
-		// fmt.Println("Going up")
 		newY = max(curPoint.y - 1, 0)
-		// return Point{curPoint.x, max(curPoint.y - 1, 0)}
 	case "D":
-		// fmt.Println("Going down")
 		newY = min(curPoint.y + 1, height - 1)
-		// return Point{curPoint.x, min(curPoint.y + 1, HEIGHT - 1)}
 	case "L":
-		// fmt.Println("Going left")
 		newX = max(curPoint.x - 1, 0)
-		// return Point{max(curPoint.x - 1, 0), curPoint.y}
 	case "R":
-		// fmt.Println("Going right")
 		newX = min(curPoint.x + 1, width - 1)
-		// return Point{min(curPoint.x + 1, WIDTH - 1), curPoint.y}
 	}
 	if shouldMakeMove(newX, newY, keypad) {
 		return Point{newX, newY}
 	}
-	// fmt.Println("BIG ERRROR IMPOSSIBLE")
 	return curPoint
 }
 
@@ -72,20 +61,18 @@ func processLine(line string, curPoint Point) Point {
 
 	for _, c := range line {
 		curPoint = processMove(string(c), curPoint, keypad)
-		// fmt.Printf("Current position is: (%v, %v)\n", curPoint.x, curPoint.x)
 	}
-	// fmt.Printf("Final position is: (%v, %v)\n", curPoint.x, curPoint.x)
 	fmt.Printf("Letter in the code: %v\n", keypad[curPoint.y][curPoint.x])
 	return curPoint
 }
 
 func main() {
 	input := retrieveInput()
-	
 
 	// Starts at 5
 	curPoint := Point{1, 1}
 	for _, val := range input {
 		curPoint = processLine(val, curPoint)
 	}
+
 }
